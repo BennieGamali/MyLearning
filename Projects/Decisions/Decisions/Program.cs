6b51b55b589be5f594af78fbb41d8e6faf27756f
Benedikthe <bengodsan@gmail.com>
2015-03-06 00:18:37 +0100

    Changely change

---
+++ Projects/Decisions/Decisions/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Please type something and press the Enter key.");
+            //string userValue;
+            //userValue = Console.ReadLine();
+            //Console.WriteLine("You typed: " + userValue);
+
+            Console.WriteLine("Would you prefer what's behind door number 1, 2 or 3?");
+            string userValue = Console.ReadLine();
+
+           
+            //string message = "";
+
+            //if (userValue == "1")
+            //    message = "You won a car!";
+            //else if (userValue == "2")
+            //    message = "You won a cat!";
+            //else if (userValue == "3")
+            //    message = "You won a can!";
+            //else 
+            //    message = "Retard!";
+
+            //Console.WriteLine(message);
+
+            string message = (userValue != "1") ? "boat" : "strand of lint";
+            Console.WriteLine("You won a {0}", message);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Projects/UnderstandingArrays/UnderstandingArrays/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int[] numbers = new int[5];
+            
+            numbers[0] = 4;
+            numbers[1] = 8;
+            numbers[2] = 15;
+            numbers[3] = 16;
+            numbers[4] = 23;
+
+            Console.WriteLine(numbers[1].ToString());
+            Console.ReadLine();
+            
+
+            int[] numbers = new int[] { 4, 8, 15, 16, 23, 42 };
+
+            Console.WriteLine(numbers[1].ToString());
+            Console.ReadLine();
+            
+
+            string[] sentences = new string[] { "Dino is cool!", "Keppet is cute!", "Spock is handsome!", "Grilka is beautiful!" };
+
+            foreach (string sentence in sentences)
+            {
+                Console.WriteLine(sentence);
+            }
+            Console.ReadLine();
+            */
+
+            string zig = "We are the playmomen! Oeooh! We come from Taiwan" +
+                "And you know you want to play with us.";
+
+            char[] charArray = zig.ToCharArray();
+            Array.Reverse(charArray);
+
+            foreach (char zigChar in charArray)
+                Console.Write(zigChar);
+
+            Console.ReadLine();
+
+        }
+    }
+}
